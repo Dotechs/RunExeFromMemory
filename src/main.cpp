54cc81751fb2ab0cefb9d912a13ed2523850cef3
Dotechs <dot.tech.9943@gmail.com>
2025-02-25 02:34:18 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,8 @@ int main() {
 
 #ifdef _WIN64
     std::vector<uint8_t> bytes = ReadIntoBuffer("C:\\Windows\\System32\\cmd.exe"); //System32 cmd is x64 exe XD.
-    //std::vector<uint8_t> bytes = ReadIntoBuffer("D:\\Apps\\Reversing\\x64dbg\\release\\x64\\x64dbg.exe");
 #else
     std::vector<uint8_t> bytes = ReadIntoBuffer("C:\\Windows\\SysWOW64\\cmd.exe");
-    //std::vector<uint8_t> bytes = ReadIntoBuffer("D:\\Apps\\Reversing\\x64dbg\\release\\x32\\x32dbg.exe");
 #endif
 
     std::thread MyExeThread(RunExe, std::ref(bytes));
